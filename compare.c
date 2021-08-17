ce539b0c2fe488a9c2f9cd7649a8653b54d7153d
Dalia <a01635883@itesm.mx>
2021-08-17 05:07:30 +0000

    tarea 1

---
+++ compare.c
@@ -0,0 +1,13 @@
+int the_biggest(int x, int y){
+    if(x>y)
+        return x;
+    else
+        return y;
+}
+
+int the_smallest(int x, int y){
+    if(x<y)
+        return x;
+    else
+        return y;
+}
